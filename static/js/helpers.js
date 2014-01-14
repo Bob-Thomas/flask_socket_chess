ac58186391a4e4fc5398e6259b19d6722333c918
Bob-Thomas <noob007>
2014-01-14 08:50:41 +0100

    fixed online saving

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -32,81 +32,49 @@ app.helper.parseBoard = function parseBoard(build,team){
         board = build.split(','),
 
             console.log("build board " +board.length)
-            console.log("building board of team " + team)
+        console.log("building board of team " + team)
+
+        for(var pieces =0; pieces <= board.length;pieces+=1){
+
+            if(pieces % 8 === 0 && pieces !== 0){
+                newBoard.push(rowString)
+                rowString = []
+            }
+            rowString.push(board[pieces])
+
+        }
         if(app.team == 'black' && team == 'white'){
-            board.reverse()
-            for(var h = 0; h <= board.length;h+=1){
-                if(board[h][0] =='w'){
-                    if(board[h][2]){
-                        var number = board[h][2];
-                        if(board[h][1] != 'P'){
-                            switch(number){
-                                case '1':
-                                    board[h] = board[h].replace(number,'2')
-                                case '2':
-                                    board[h] =  board[h].replace(number,'1')
+            console.log("swagaroni " +newBoard.length)
+            newBoard = newBoard.reverse()
+            for(var i = 0; i < newBoard.length; i++){
+                newBoard[i] = newBoard[i].reverse()
+            }
 
-                            }
-                        }
-                        else{
-                            var newNumber = 7-parseInt(number)
-                            board[h] =  board[h].replace(number,newNumber)
-                        }
-
-                    }
-                }
 
         }
-        }
+
         if(app.team == 'white' && team == 'black'){
-            for(var h = 0; h <= board.length;h+=1){
-                if(board[h][0] == 'b'){
-                    if(board[h][2] && board[h][0] =='w'){
-                        var number = board[h][2];
-                        if(board[h][1] != 'P'){
-                            switch(number){
-                                case '1':
-                                    board[h] = board[h].replace(number,'2')
-                                case '2':
-                                    board[h] =  board[h].replace(number,'1')
+            for(var i = 0; i < newBoard.length; i++){
+                newBoard[i] = newBoard[i].reverse()
+            }
 
-                            }
+        }
+        for(var i = 0; i < 8 ; i+=1){
+            for(var j =0; j < 8; j+=1) {
+                for ( var team in app.pieceSet){
+                    for(var object in app.pieceSet[team]){
+                        var
+                            piece = app.pieceSet[team][object]
+                            ;
+                        if(newBoard[i][j] == piece.getId()){
+                            piece.setPosition([i,j])
                         }
-                        else{
-                            var newNumber = 7-parseInt(number)
-                            board[h] =  board[h].replace(number,newNumber)
-                        }
-
                     }
                 }
 
-        }
-        }
-            for(var pieces =0; pieces <= board.length;pieces+=1){
-
-                if(pieces % 8 === 0 && pieces !== 0){
-                    newBoard.push(rowString)
-                    rowString = []
-                }
-                rowString.push(board[pieces])
-
             }
-            for(var i = 0; i < 8 ; i+=1){
-                for(var j =0; j < 8; j+=1) {
-                    for ( var team in app.pieceSet){
-                        for(var object in app.pieceSet[team]){
-                            var
-                                piece = app.pieceSet[team][object]
-                                ;
-                            if(newBoard[i][j] == piece.getId()){
-                                piece.setPosition([i,j])
-                            }
-                        }
-                    }
 
-                }
-
-            }
+        }
 
 
     }else{
@@ -137,12 +105,12 @@ app.helper.parseBoard = function parseBoard(build,team){
         }
 
 
-            if(app.team == 'white'){
-                return board.toString()
-            }
+        if(app.team == 'white'){
+            return board.toString()
+        }
         else{
             return board.reverse().toString()
-            }
+        }
 
 
     }
